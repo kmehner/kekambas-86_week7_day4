e1b52292b599eb34fc29e17bd678a6d96f131d31
Katelyn Mehner <kmehner@gmail.com>
2022-04-07 14:36:43 -0600

    Fetches from Country API

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 // Create a Scope to limit my variables
 
 {
@@ -67,4 +66,85 @@
             body.className = `bg-${myButtonColors[i]}`;
         })
     }
+}
+
+// Get country info and display on page
+{
+    // Grab the form
+    let form = document.getElementById('countryForm');
+    
+    // Create function to handle Submit Event
+    async function handleSubmit(e){
+        e.preventDefault();
+        let countryName = e.target.countryName.value
+        // Here is where we will make the request to get the data
+        let country = await getCountryInfo(countryName)
+        console.log(country);
+        // Here is where we will build the element to display
+        await buildCountryCard(country);
+        e.target.countryName.value = '';
+    }
+
+    // function that accepts country name and returns a country object
+    async function getCountryInfo(countryName){
+        try{
+            let res = await fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+            let data = await res.json()
+            return data[0]
+        } catch(e){
+            console.error(e)
+        }
+    }
+
+    // function to build the card for the country
+    async function buildCountryCard(country){
+        // Create card div
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        // Create a top image
+        const image = document.createElement('img')
+        image.className = 'card-img-top';
+        image.src = country.flags.png;
+        // Add image to the card div
+        card.append(image);
+
+        // Create card body div
+        const cardBody = document.createElement('div');
+        cardBody.className = 'card-body'
+
+        // Create country name and population elements
+        const countryTitle = document.createElement('h5');
+        countryTitle.className = 'card-title';
+        countryTitle.innerHTML = country.name.official
+
+        const population = document.createElement('p');
+        population.className = 'card-text';
+        population.innerHTML = `Population: ${country.population.toLocaleString('en-US')}`;
+
+        // Append name and pop to card body
+        cardBody.append(countryTitle);
+        cardBody.append(population);
+
+        // Add card body to card div
+        card.append(cardBody);
+
+        // Create column div
+        const col = document.createElement('div');
+        col.className = 'col-12 col-md-6 col-lg-3';
+
+        // Add the card to the column
+        col.append(card);
+
+        // Get the country Display row
+        const countryDisplayRow = document.getElementById('countryDisplay');
+
+        // Add new column to the display
+        countryDisplayRow.append(col);
+
+    }
+
+    // Add submit event listener to form
+    form.addEventListener('submit', handleSubmit);
+
 }
